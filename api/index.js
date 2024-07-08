2a0996c3305ef826d28c9fc4f8afbc33cf510c1a
Abhishek Kumar <abhishek.andri05@gmail.com>
2024-07-08 18:34:46 +0530

    complete sign up page functionality

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ app.use((err, req, res, next)=>{
     const statusCode = err.statusCode || 500
     const message = err.message || "Internal server error"
     return res.status(statusCode).json({
-        success:"false",
+        success:false,
         message,
         statusCode
     })
